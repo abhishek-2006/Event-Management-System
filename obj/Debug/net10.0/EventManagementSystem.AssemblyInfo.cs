574c43a5ba3a2e7a7ad875fece4cb155d138fdcb
abhishek-2006 <shahabhishek409@gmail.com>
2026-03-22 23:12:17 +0530

    refactor: Remove User model as part of system redesign

--- obj/Debug/net10.0/EventManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net10.0/EventManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+139c43408e9385b4fe51896d34d7476840eb732b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb9e28ec62a08de08ae0bfccf8d6ea62a20fec3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
