58397ec5b38b31e200868e95b455142eef48e2e5
abhishek-2006 <shahabhishek409@gmail.com>
2026-03-30 09:53:36 +0530

    fix: Update applicationUrl in launchSettings.json for better accessibility

--- obj/Debug/net10.0/EventManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net10.0/EventManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f42169d3b52c345bf2b64f4a87d4466d5fac35bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d591f42ebe7fb4456eeb327c4556ab7b6cb83b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
